f4a96a0d6ace5e65a1d6aa9e7b98646903f5aa24
mack millar <mackmillar@gmail.com>
2021-01-20 12:01:57 +0000

    added methods for display boookings

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>Hotel bookings</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  components: {},
+  data() {
+    return {
+      bookings: [],
+    };
+  },
+  mounted() {
+    this.fetchBookings();
+  },
+  methods: {
+    fetchBookings() {
+      BookingsService.getBookings().then(
+        (bookings) => (this.bookings = bookings)
+      );
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ client/src/components/BookingGrid.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div id="bookingsGrid">
+        <booking-card v-for="(booking,index) in bookings" :key="index" :booking="booking" />
+  </div>
+</template>
+
+<script>
+import BookingCard from "@/BookingCard.js";
+export default {
+  name: "bookings-grid",
+  components: {
+    "booking-card": BookingCard,
+  },
+  props: ["bookings"],
+};
+</script>
+
+<style>
+</style>
